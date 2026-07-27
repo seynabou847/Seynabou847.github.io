1e9878e0837e101c862659ba6759de61ae301f92
seynabou847 <zeynoundiaye847@gmail.com>
2026-07-27 17:49:35 +0000

    Create initial HTML structure for the site

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <title>Mon site</title>
+</head>
+<body>
+  <h1>Bienvenue sur mon site !</h1>
+  <p>Je m'appelle Seynabou et voici ma première page web.</p>
+</body>
+</html>
